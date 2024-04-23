dedf019dec6fc73e73f0bc8c2419ae64281b2e27
Sandy Gomez <sgomez1029@gmail.com>
2024-04-23 12:15:03 -0700

    Multiple View in Storyboards (UCSD Futures)

--- MultipleViewsStoryboard/ViewController.swift
+++ MultipleViewsStoryboard/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func cancel(_ unwindSegue: UIStoryboardSegue) {}
 
 }
 
